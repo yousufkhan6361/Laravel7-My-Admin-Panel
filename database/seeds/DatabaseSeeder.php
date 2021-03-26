c59c763c3b1391492e795913c7ece5e8b79f72ba
sajjad khan <sajjadkhan6361@gmail.com>
2021-03-26 05:15:54 +0500

    did changes in DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(CategoriesSeeder::class);
+        $this->call(countriesSeeder::class);
     }
 }
