15e1b65e305816948cbf62aaa222e6dc3925f580
sajjad khan <sajjadkhan6361@gmail.com>
2022-01-11 20:42:58 +0500

    dashboard changes

--- resources/admin/views/dashboard.blade.php
+++ resources/admin/views/dashboard.blade.php
@@ -53,7 +53,7 @@ Admin | Dashboard
               <div class="icon">
                 <i class="fas fa-shopping-cart"></i>
               </div>
-              <a href="#" class="small-box-footer">
+              <a href="{{url('admin/order')}}" class="small-box-footer">
                 More info <i class="fas fa-arrow-circle-right"></i>
               </a>
             </div>
